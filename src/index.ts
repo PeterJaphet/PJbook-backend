ce5848bc99fd90c6ed106915baf523724adadcc4
Peter Japhet <peterjaphet111@gmail.com>
2023-08-07 20:53:14 +0300

    added middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import 'dotenv/config'
+import userRoutes from '../src/routes/userRoutes'
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 
 const port = process.env.PORT || 5000
 
@@ -17,8 +19,12 @@ app.use(cors({
 app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
+app.use(notFound)
+app.use(errorHandler)
 
 const server = http.createServer(app);
+
+app.use('/api/users', userRoutes)
 
 app.get('/', (req,res) => res.send('Server is ready!'));
 
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { authUser } from "../controllers/userController";
+import express from "express";
+
+const router = express.Router();
+
+router.post('/auth', authUser)
+
+
+
+
+export default router;
