81108d8ccd6260a9544785469165bc0067e23300
Peter Japhet <peterjaphet111@gmail.com>
2023-09-30 19:13:16 +0300

    added logger and structured the project architecture

--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,18 @@ import 'dotenv/config'
 import userRoutes from '../src/routes/userRoutes'
 import { notFound, errorHandler } from './middleware/errorMiddleware';
 import connectDB from './config/db';
+import createApp from './app';
+import logger from './utils/logger';
 
 
 connectDB();
 
 const port = process.env.PORT || 5000
 
-const app = express();
-
-app.use(cors({
-    credentials:true,
-}))
-
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
+const app = createApp();
 
 const server = http.createServer(app);
 
-app.use('/api/users', userRoutes)
-
-
-app.get('/', (req,res) => res.send('Server is ready!'));
-
-
-app.use(notFound)
-app.use(errorHandler)
-
 server.listen(port, ()=>{
-    console.log(`Server runnig on http://localhost:${port}`)
+    logger.info(`Server runnig on http://localhost:${port}`)
 })
