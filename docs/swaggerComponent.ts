7a9864d3cd13f8f3e8ea28fec9102e88124cb941
UzoAronu <uzo.code123@gmail.com>
2024-03-15 10:35:35 +0100

    implemented update user, update profile picture, Reset password, ChangePassword

--- docs/swaggerComponent.ts
+++ docs/swaggerComponent.ts
@@ -1,5 +1,148 @@
 /**
  * @openapi
+ * '/users/forgot-password':
+ *    get:
+ *      tags:
+ *        - user
+ *      summary: shows a html page to insert user email
+ *      description: Insert user Email so that a reset email can be sent to user
+ *      operationId: forgot-password
+ *      parameters: []
+ *      
+ *      responses:
+ *        default:
+ *           description: successful operation
+ *           
+ * 
+ * '/users/update-profile-picture':
+ *    post:
+ *      tags:
+ *        - user
+ *      summary: updates profile picture 
+ *      description: updates user's profile picture
+ *      operationId: update-profile-picture
+ *      requestBody:
+ *        required: true
+ *        description: input User's email and Avatar
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateProfilePicture'
+ *          application/xml:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateProfilePicture'
+ *          application/x-www-form-urlencoded:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateProfilePicture'
+ *      responses:
+ *        default:
+ *           description: successful operation
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/UpdateProfilePictureResponse'
+ *             application/xml:
+ *               schema:
+ *                 $ref: '#/components/schemas/UpdateProfilePictureResponse'
+ * 
+ * 
+ * 
+ * '/users//update-user-profile':
+ *    post:
+ *      tags:
+ *        - user
+ *      summary: updates user's Profile 
+ *      description: Updates User's Profile
+ *      operationId: update-user-profile
+ *      requestBody:
+ *        required: true
+ *        description: input required User's details to be changed
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateUserProfile'
+ *          application/xml:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateUserProfile'
+ *          application/x-www-form-urlencoded:
+ *            schema:
+ *              $ref: '#/components/schemas/UpdateUserProfile'
+ *      responses:
+ *        default:
+ *           description: successful operation
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/UpdateUserProfileResponse'
+ *             application/xml:
+ *               schema:
+ *                 $ref: '#/components/schemas/UpdateUserProfileResponse'
+ * 
+ * 
+ * '/users/forgot-password-processor':
+ *    post:
+ *      tags:
+ *        - user
+ *      summary: sends password reset form to user's email 
+ *      description: validates User's email, sends reset form to user's email
+ *      operationId: forgot-password-processor
+ *      requestBody:
+ *        required: true
+ *        description: input User's email
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ForgotPasswordUserEmail'
+ *          application/xml:
+ *            schema:
+ *              $ref: '#/components/schemas/ForgotPasswordUserEmail'
+ *          application/x-www-form-urlencoded:
+ *            schema:
+ *              $ref: '#/components/schemas/ForgotPasswordUserEmail'
+ *      responses:
+ *        default:
+ *           description: successful operation
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/ForgotPasswordUserEmailResponse'
+ *             application/xml:
+ *               schema:
+ *                 $ref: '#/components/schemas/ForgotPasswordUserEmailResponse'
+ * 
+ * 
+ * '/users/reset-password-processor':
+ *    post:
+ *      tags:
+ *        - user
+ *      summary: resets password with information from email 
+ *      description: resets passsword
+ *      operationId: reset-password-processor
+ *      requestBody:
+ *        required: true
+ *        description: input User's email, newPassword and confirmPassword
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ResetPasswordInputs'
+ *          application/xml:
+ *            schema:
+ *              $ref: '#/components/schemas/ResetPasswordInputs'
+ *          application/x-www-form-urlencoded:
+ *            schema:
+ *              $ref: '#/components/schemas/ResetPasswordInputs'
+ *      responses:
+ *        default:
+ *           description: successful operation
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/ResetPasswordInputsResponse'
+ *             application/xml:
+ *               schema:
+ *                 $ref: '#/components/schemas/ResetPasswordInputsResponse'
+ * 
+ * 
  * '/users':
  *    post:
  *      tags:
@@ -189,6 +332,142 @@
 
  * components:
  *   schemas:
+ *     ForgotPasswordUserEmail:
+ *      type: object
+ *      required: 
+ *        - email
+ *        - admin
+ *        
+ *      properties:      
+ *        email:
+ *          type: string
+ *          example: uzo@gmail.com
+ *        admin:
+ *          type: boolean
+ *          example: true
+ *        
+ *     ForgotPasswordUserEmailResponse:
+ *       type: object
+ *       properties:
+ *         firstName:
+ *           type: string         
+ *         lastName:
+ *           type: string          
+ *         email:
+ *           type: string        
+ *         _id:
+ *           type: string        
+ *         createdAt:
+ *           type: string         
+ *         updatedAt:
+ *           type: string         
+ *       
+ * 
+ *     UpdateProfilePicture:
+ *      type: object
+ *      required: 
+ *        - email
+ *        - avatar
+ *        
+ *      properties:      
+ *        email:
+ *          type: string
+ *          example: uzo@gmail.com
+ *        avatar:
+ *          type: string
+ *          
+ *        
+ *     UpdateProfilePictureResponse:
+ *       type: object
+ *       properties:
+ *         avatar:
+ *           type: string        
+ *         _id:
+ *           type: string        
+ *         createdAt:
+ *           type: string         
+ *         updatedAt:
+ *           type: string         
+
+
+ *     UpdateUserProfile:
+ *      type: object
+ *      required: 
+ *        - email
+ *        - firstName
+ *        - lastName
+ *        - dob
+ *        - role
+ *        - pnoneNumber
+ *      properties:
+*        firstName:
+ *          type: string
+ *          example: uzo
+ *        lastName:
+ *          type: string
+ *          example: Aronu
+ *        email:
+ *          type: string
+ *          example: uzo@gmail.com
+ *        dob:
+ *          type: string
+ *          example: '14th may'
+ *        role:
+ *          type: string
+ *          example: admin
+ *        phoneNumber:
+ *          type: string
+ *          example: 12345678910
+ *        verified:
+ *          type: boolean
+ *          example: true or false
+ *        isActive:
+ *          type: boolean
+ *          example: true or false
+ 
+ *     UpdateUserProfileResponse:
+ *       type: object
+ *       properties:
+ *        firstName:
+ *          type: string
+
+ *        lastName:
+ *          type: string
+
+ *        email:
+ *          type: string
+
+ *        dob:
+ *          type: string
+
+ *        password:
+ *          type: string
+
+ *        role:
+ *          type: string
+
+ *        phoneNumber:
+ *          type: string
+ 
+ *        avatar: 
+ *          type: string
+ *        address:
+ *          type: string
+
+ *        verified:
+ *          type: boolean
+
+ *        isActive:
+ *          type: boolean
+ *                
+ 
+ *        createdAt:
+ *           type: string         
+ *        updatedAt:
+ *           type: string         
+         
+       
+ * 
  *     User:
  *      type: object
  *      required: 
@@ -223,9 +502,16 @@
  *        address:
  *          type: string
  *          example: Barclays-abuja
+ *        verified:
+ *          type: boolean
+ *          example: true or false
+ *        isActive:
+ *          type: boolean
+ *          example: true or false
+ *          
  *     UserResponse:
- *       type: object
- *       properties:
+ *      type: object
+ *      properties:
  *         firstName:
  *           type: string         
  *         lastName:
@@ -250,6 +536,42 @@
  *           type: string
  *         isActive:
  *           type: boolean   
+ *         verified:
+ *          type: boolean
+ * 
+ *     ResetPasswordInputs:
+ *      type: object
+ *      required: 
+ *        - email
+ *        - password
+ *        - role
+ *        - pnoneNumber
+ *      properties:
+
+ *        email:
+ *          type: string
+ *          example: uzo@gmail.com
+ *        newPassword:
+ *          type: string
+ *          example: '123456'
+ *        confirmPassword:
+ *          type: string
+ *          example: '123456'
+ 
+ *     ResetPasswordInputsResponse:
+ *       type: object
+ *       properties:
+         
+ *         email:
+ *           type: string        
+ 
+ *         createdAt:
+ *           type: string         
+ *         updatedAt:
+ *           type: string         
+ *         password:
+ *           type: string           
+ 
  * 
  *     Send-otp:
  *      type: object
